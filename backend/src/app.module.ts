65041aaef2443d542a1b3790d6d34452c9fa8ee8
Victoria <victoria.yamashita@estudante.ufjf.br>
2025-06-29 20:23:32 -0300

    feat(database): integrate TypeORM with PostgreSQL for database management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'tasks_db',
+      autoLoadEntities: true, // Carrega automaticamente as entidades definidas
+      synchronize: true, // Sincroniza o esquema do banco de dados com as entidades (somente em DEV)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
