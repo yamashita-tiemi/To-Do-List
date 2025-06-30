1f86e05da703609dce7c0d075b09e7649b545916
Victoria <victoria.yamashita@estudante.ufjf.br>
2025-06-30 16:56:49 -0300

    feat(cors): enable CORS for localhost

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
